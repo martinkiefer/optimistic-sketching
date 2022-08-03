1fa7ad2ddf844be74e6953f565e1ffaa96c6d750
Martin Kiefer <martin.kiefer@tu-berlin.de>
2022-08-04 01:15:32 +0200

    Added download and extract script for nyt.

---
+++ data/generate_nyt.sh
@@ -0,0 +1,6 @@
+rm -rf nyt_keys.* nyt_values.*
+wget https://tubcloud.tu-berlin.de/s/oPQzYAxzZ6bPGwM/download/nyt_keys.bin.gz  -O ./nyt_keys.bin.gz
+gunzip nyt_keys.bin.gz
+
+wget https://tubcloud.tu-berlin.de/s/5C3bryrzMcDnfsK/download/nyt_values.bin.gz  -O ./nyt_values.bin.gz
+gunzip nyt_values.bin.gz
