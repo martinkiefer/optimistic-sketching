148a9eef91f730acf119d5d1c3c07953c79af953
Martin Kiefer <martin.kiefer@tu-berlin.de>
2022-08-04 01:19:18 +0200

    Added download and extract script for caida.

---
+++ data/generate_caida.sh
@@ -0,0 +1,7 @@
+rm -rf caida_keys.* caida_values.*
+wget https://tubcloud.tu-berlin.de/s/HMbKYEWPtSap6f9/download/caida_keys.bin.gz  -O ./caida_keys.bin.gz
+gunzip caida_keys.bin.gz
+
+wget https://tubcloud.tu-berlin.de/s/Jor96j5zCb882HA/download/caida_values.bin.gz  -O ./caida_values.bin.gz
+gunzip caida_values.bin.gz
+
