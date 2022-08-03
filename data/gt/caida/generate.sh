941c82e0c074d967af5e1e69634f54156756fe45
Martin Kiefer <martin.kiefer@tu-berlin.de>
2022-08-04 01:31:21 +0200

    Added groundtruth script for real-world datasets.

---
+++ data/gt/caida/generate.sh
@@ -0,0 +1,5 @@
+rm -rf *.tar.gz *.bin
+
+wget https://tubcloud.tu-berlin.de/s/9aBT6pCLjxD38dN/download/caida_gt.tar.gz  -O ./caida_gt.tar.gz
+tar -xvzf caida_gt.tar.gz
+rm caida_gt.tar.gz
---
+++ data/gt/nyt/generate.sh
@@ -0,0 +1,5 @@
+rm -rf *.tar.gz *.bin
+
+wget https://tubcloud.tu-berlin.de/s/KaLAJdzcekS62Lf/download/nyt_gt.tar.gz  -O ./nyt_gt.tar.gz
+tar -xvzf nyt_gt.tar.gz
+rm nyt_gt.tar.gz
