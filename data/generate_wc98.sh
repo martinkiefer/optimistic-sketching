c76e3daf40fea57544c5b3f40021a89e5758380b
Martin Kiefer <martin.kiefer@tu-berlin.de>
2022-08-04 01:21:57 +0200

    Added download and extract script for wc98.

---
+++ data/generate_wc98.sh
@@ -0,0 +1,6 @@
+rm -rf wc98_keys.* wc98_values.*
+wget https://tubcloud.tu-berlin.de/s/gP7YWtM4ZS8w4RB/download/wc98_keys.bin.gz  -O ./wc98_keys.bin.gz
+gunzip wc98_keys.bin.gz
+
+wget https://tubcloud.tu-berlin.de/s/YBoaRdHDnrMZ645/download/wc98_values.bin.gz  -O ./wc98_values.bin.gz
+gunzip wc98_values.bin.gz
