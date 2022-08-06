03cce4594907a5bbdf16534b30504a1e2de63abf
Martin Kiefer <martin.kiefer@tu-berlin.de>
2022-08-06 02:07:32 +0200

    For some reason, the old links from TUBcloud stopped working.

--- Data/generate_caida.sh
+++ Data/generate_caida.sh
@@ -1,5 +1,5 @@
 rm -rf caida_keys.* caida_values.*
-wget https://tubcloud.tu-berlin.de/s/HMbKYEWPtSap6f9/download/caida_keys.bin.gz  -O ./caida_keys.bin.gz
+wget https://tubcloud.tu-berlin.de/s/dg5pNd2fdcJ93mc/download/caida_keys.bin.gz  -O ./caida_keys.bin.gz
 gunzip caida_keys.bin.gz
 
 wget https://tubcloud.tu-berlin.de/s/Jor96j5zCb882HA/download/caida_values.bin.gz  -O ./caida_values.bin.gz
--- Data/generate_nyt.sh
+++ Data/generate_nyt.sh
@@ -2,5 +2,5 @@ rm -rf nyt_keys.* nyt_values.*
 wget https://tubcloud.tu-berlin.de/s/oPQzYAxzZ6bPGwM/download/nyt_keys.bin.gz  -O ./nyt_keys.bin.gz
 gunzip nyt_keys.bin.gz
 
-wget https://tubcloud.tu-berlin.de/s/5C3bryrzMcDnfsK/download/nyt_values.bin.gz  -O ./nyt_values.bin.gz
+wget https://tubcloud.tu-berlin.de/s/bYkr7d5P6ks4obn/download/nyt_values.bin.gz  -O ./nyt_values.bin.gz
 gunzip nyt_values.bin.gz
