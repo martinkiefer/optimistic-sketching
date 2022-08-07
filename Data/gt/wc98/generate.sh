a0d0fbea2450991388fb97b30231ee1e9436f202
Martin Kiefer <martinkiefer@boxm.de>
2022-08-08 01:41:09 +0200

    Fixed broken links due to typical TUB cloud bs.

--- Data/gt/wc98/generate.sh
+++ Data/gt/wc98/generate.sh
@@ -1,5 +1,5 @@
 rm -rf *.tar.gz *.bin
 
-wget https://tubcloud.tu-berlin.de/s/Dyd4QSHeAKENZN4/download/wc98_gt.tar.gz  -O ./wc98_gt.tar.gz
+wget https://tubcloud.tu-berlin.de/s/DNidmScriLLnRem/download/wc98_gt.tar.gz  -O ./wc98_gt.tar.gz
 tar -xvzf wc98_gt.tar.gz
 rm wc98_gt.tar.gz
