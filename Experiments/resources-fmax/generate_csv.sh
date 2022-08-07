85d92022b6bb36f6f013c3be78130fa40825c2e2
Martin Kiefer <martinkiefer@boxm.de>
2022-08-07 14:34:41 +0200

    Fixed path error in generate_csv.sh

--- Experiments/resources-fmax/generate_csv.sh
+++ Experiments/resources-fmax/generate_csv.sh
@@ -1,5 +1,4 @@
 dir=$(pwd)
-script_dir=$(dirname "$0")
 
 cd $1
 for d in */; do 
@@ -7,8 +6,8 @@ for d in */; do
     if test -f "$d/utilization_hierarchy.rpt"; then
         c=$(echo $d | tr " " ,)
         c=$(echo $c | tr . ,)
-        t=$(python $script_dir/timing_to_csv.py "$d/cproject.runs/impl_1/tl_timing_summary_routed.rpt")
-        for l in $(python $script_dir/util_to_csv.py "$d/utilization_hierarchy.rpt"); do
+        t=$(python ${dir}/timing_to_csv.py "$d/cproject.runs/impl_1/tl_timing_summary_routed.rpt")
+        for l in $(python ${dir}/util_to_csv.py "$d/utilization_hierarchy.rpt"); do
             echo $c,$l,$t
         done
     fi
