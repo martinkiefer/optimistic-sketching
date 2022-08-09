b6c812db4e5753d6a4c63e211bca696018afbd11
Martin Kiefer <martinkiefer@boxm.de>
2022-08-10 00:15:38 +0200

    Fixed a link Caida data set.

--- Data/generate_caida.sh
+++ Data/generate_caida.sh
@@ -1,7 +1,9 @@
 rm -rf caida_keys.* caida_values.*
-wget https://tubcloud.tu-berlin.de/s/dg5pNd2fdcJ93mc/download/caida_keys.bin.gz  -O ./caida_keys.bin.gz
+wget https://tubcloud.tu-berlin.de/s/CypXyJbkb2yK4Yp/download/caida_keys.bin.gz  -O ./caida_keys.bin.gz
 gunzip caida_keys.bin.gz
 
+
+://tubcloud.tu-berlin.de/s/
 wget https://tubcloud.tu-berlin.de/s/Jor96j5zCb882HA/download/caida_values.bin.gz  -O ./caida_values.bin.gz
 gunzip caida_values.bin.gz
 
