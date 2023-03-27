9f53c570f29e2daa4fa0e90e570fe775efb03f0b
Martin Kiefer <martin.kiefer@tu-berlin.de>
2023-03-27 15:30:26 +0200

    Added outstanding changes for throughput experiment baselines.

--- Experiments/throughput/GPU/run_experiment.sh
+++ Experiments/throughput/GPU/run_experiment.sh
@@ -3,10 +3,12 @@ for df in zipf_1.05 zipf_1.1 zipf_1.5 nyt wc98 caida uniform; do
     for i in {1..10}; do
         ./row-by-row 2 $(( 4096*32 )) ../../../Data/${df}_keys.bin ../../../Data/${df}_values.bin >> ${df}.csv
         ./row-by-row 2 $(( 4096*16 )) ../../../Data/${df}_keys.bin ../../../Data/${df}_values.bin >> ${df}.csv
+        ./row-by-row 2 $(( 4096*5 )) ../../../Data/${df}_keys.bin ../../../Data/${df}_values.bin >> ${df}.csv
 
         for p in {0..14}; do 
             ./replicated 2 $(( 4096*32 )) $((2 ** $p)) ../../../Data/${df}_keys.bin ../../../Data/${df}_values.bin >> ${df}.csv
             ./replicated 2 $(( 4096*16 )) $((2 ** $p)) ../../../Data/${df}_keys.bin ../../../Data/${df}_values.bin >> ${df}.csv
+            ./replicated 2 $(( 4096*5 )) $((2 ** $p)) ../../../Data/${df}_keys.bin ../../../Data/${df}_values.bin >> ${df}.csv
         done
     done
 done
